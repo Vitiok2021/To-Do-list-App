8b70e4853be862ef5aef48863f05f67defbeb26f
Vitiok2021 <92989701+Vitiok2021@users.noreply.github.com>
2025-04-18 07:33:44 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,82 @@
+document.querySelector('.to-do__btn').onclick = addNewTask
+
+function addNewTask() {
+  const taskTitle = document.querySelector('#task').value
+  const taskPriority = document.querySelector('#priority').value
+
+  const taskAnswerDiv = document.createElement('div')
+  const span = document.createElement('span')
+  taskAnswerDiv.append(span)
+  span.innerText = `${taskTitle} - ${taskPriority}`
+
+  const changeBtn = document.createElement('button')
+  changeBtn.innerText = 'Change'
+  changeBtn.classList.add('change-btn')
+  changeBtn.onclick = onTaskChange
+  taskAnswerDiv.append(changeBtn)
+
+  const delBtn = document.createElement('button')
+  delBtn.innerText = 'Delete'
+  delBtn.classList.add('del-btn')
+  delBtn.onclick = onTaskDelete
+  taskAnswerDiv.append(delBtn)
+
+  document.querySelector('.to-do__answers').append(taskAnswerDiv)
+}
+function onTaskDelete() {
+  console.log(this.parentElement)
+  this.parentElement.remove()
+}
+function onTaskChange() {
+  const taskDiv = this.parentElement
+  taskDiv.querySelector('.change-btn').style.display = 'none'
+  taskDiv.querySelector('.del-btn').style.display = 'none'
+
+  const span = taskDiv.querySelector('span')
+  const [title, priority] = span.innerText.split(' - ')
+  span.innerText = ''
+
+  const priorityInput = document.createElement('input')
+  taskDiv.prepend(priorityInput)
+  priorityInput.classList.add('new-priority')
+  priorityInput.value = priority
+
+  const titleInput = document.createElement('input')
+  taskDiv.prepend(titleInput)
+  titleInput.classList.add('new-task')
+  titleInput.value = title
+
+  const saveBtn = document.createElement('button')
+  saveBtn.onclick = onSaveInput
+  saveBtn.innerText = 'Save'
+  taskDiv.append(saveBtn)
+}
+
+function onSaveInput() {
+  const taskDiv = this.parentElement
+  const newTitleValue = taskDiv.querySelector('.new-task')
+  const newPriorityValue = taskDiv.querySelector('.new-priority')
+
+  console.log(newTitleValue, newPriorityValue)
+
+  const span = document.createElement('span')
+
+  span.innerText = `${newTitleValue.value} - ${newPriorityValue.value}`
+
+  taskDiv.prepend(span)
+  newTitleValue.remove()
+  newPriorityValue.remove()
+  this.remove()
+
+  taskDiv.querySelector('.change-btn').style.display = 'inline-block'
+  taskDiv.querySelector('.del-btn').style.display = 'inline-block'
+}
+
+document.querySelector('.to-do__clear-all').onclick = clearAll
+
+function clearAll() {
+  const btn = this
+  const list = btn.closest('.list')
+  list.lastElementChild.innerText = ''
+  console.log(list)
+}
